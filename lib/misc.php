b39ed07b8cced5e08b955b35a69f28dae5c3e954
Lucas <lucas@x3ro.de>
2011-02-25 22:45:59 +0100

    Started adding the actual CenteredPerspective template. Only markup until now, CSS will follow.

--- lib/misc.php
+++ lib/misc.php
@@ -1,15 +1,28 @@
 <?php
 
 function oldIncludeHook($name) {
-	/*old includehook*/ @include(TEMPLATE_ROOT . '/hooks/' . $name . '.html');
+	# Workaround for global variables.
+	# TODO Check if this has a performance impact, and if so, think of something nicer.
+	foreach ($GLOBALS as $key => $val) { global $$key; }
+	
+	# Did not use @include to not surpress fatal errors etc while executing hook script.
+	/*old includehook*/
+	$file = TEMPLATE_ROOT . '/hooks/' . $name . '.html';
+	if(file_exists($file)) {
+		include($file);
+		tpl_flush();
+	}
+	
 }
 
 function echoPagename() {
+	global $ID, $lang;
 	echo "[[";
 	tpl_link(wl($ID,'do=backlink'),tpl_pagetitle($ID,true),'title="'.$lang['btn_backlink'].'"');
 	echo "]]";
 }
 
 function echoLogo() {
+	global $conf;
 	tpl_link(wl(),$conf['title'],'name="dokuwiki__top" id="dokuwiki__top" accesskey="h" title="[H]"');
 }
--- main.php
+++ main.php
@@ -18,7 +18,6 @@ if (!defined('DOKU_INC')) die();
 define('TEMPLATE_ROOT', dirname(__FILE__));
 
 require_once('lib/misc.php');
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -41,96 +40,11 @@ require_once('lib/misc.php');
 		<?php oldIncludeHook('topheader'); ?>
 		
 		<div class="dokuwiki">
+			<?php oldIncludeHook('menu'); ?>
 			
 			<?php html_msgarea()?>
 			
-			<div class="stylehead">
-				<div class="header">
-					<div class="pagename">
-						<?php echoPagename(); ?>
-					</div>
-					<div class="logo">
-						<?php echoLogo(); ?>
-					</div>
-					<div class="clearer"></div>
-				</div>
-				
-				<?php oldIncludeHook('header'); ?>
-				
-				<div class="bar" id="bar__top">
-					<div class="bar-left" id="bar__topleft">
-						<?php tpl_button('edit'); ?>
-						<?php tpl_button('history'); ?>
-					</div>
-					<div class="bar-right" id="bar__topright">
-						<?php tpl_button('recent'); ?>
-						<?php tpl_searchform(); ?>
-						&nbsp;
-					</div>
-					<div class="clearer"></div>
-				</div>
-				
-				<?php if($conf['breadcrumbs']): ?>
-				<div class="breadcrumbs">
-					<?php tpl_breadcrumbs()?>
-					<?php //tpl_youarehere() //(some people prefer this)?>
-				</div>
-				<?php endif; ?>
-					
-				<?php if($conf['youarehere']): ?>
-				<div class="breadcrumbs">
-					<?php tpl_youarehere() ?>
-				</div>
-				<?php endif; ?>
-				
-			</div>
-			
-			<?php tpl_flush()?>	
-			<?php oldIncludeHook('pageheader'); ?>
-			
-			<div class="page">
-				<!-- wikipage start -->
-				<?php tpl_content()?>
-				<!-- wikipage stop -->
-			</div>
-			<div class="clearer">
-				&nbsp;
-			</div>
-			
-			<?php tpl_flush()?>
-			
-			<div class="stylefoot">
-				<div class="meta">
-					<div class="user">
-						<?php tpl_userinfo()?>
-					</div>
-					<div class="doc">
-						<?php tpl_pageinfo()?>
-					</div>
-				</div>
-				
-				<?php oldIncludeHook('pagefooter'); ?>
-				
-				<div class="bar" id="bar__bottom">
-					<div class="bar-left" id="bar__bottomleft">
-						<?php tpl_button('edit'); ?>
-						<?php tpl_button('history'); ?>
-						<?php tpl_button('revert'); ?>
-					</div>
-					<div class="bar-right" id="bar__bottomright">
-						<?php tpl_button('subscribe'); ?>
-						<?php tpl_button('admin'); ?>
-						<?php tpl_button('profile'); ?>
-						<?php tpl_button('login'); ?>
-						<?php tpl_button('index'); ?>
-						<?php tpl_button('top'); ?>&nbsp;
-					</div>
-					<div class="clearer"></div>
-				</div>
-			</div>
-			
-			<?php tpl_license(false);?>
-			
+			<?php oldIncludeHook('content'); ?>
 		</div>
 		
 		<?php oldIncludeHook('footer'); ?>
